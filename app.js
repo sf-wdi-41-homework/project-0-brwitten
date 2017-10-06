f738ee5f6545f3ab9d838134603ad5869c95340a
brwitten <bethrwitten@gmail.com>
2017-10-05 20:51:56 -0700

    added initial icons

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ console.log("I'm linked!");
 $(document).ready(function(){
 
 };
+
+// need to create an object and place it on the screen
+// need to determine and update the objects position through event listeners
+// need to determine winning position
+// need to determine tie position 
