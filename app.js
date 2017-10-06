4a65ae050aee5973039600e9080b1ddf135399d8
brwitten <bethrwitten@gmail.com>
2017-10-06 13:26:43 -0700

    two moving objects controlled with two keys

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ $(document).ready(function(){
 
   var c = document.getElementById('racetrack');
   var ctx = c.getContext("2d");
-  var car1 = ctx.fillRect(10,10,15,10);
+  var car1 = ctx.fillRect(10,20,20,40);
   var car1x = 10;
-  var car1y = 10;
-  var car2 = ctx.fillRect(10,50,15,10);
+  var car1y = 20;
+  var car2 = ctx.fillRect(10,90,20,40);
   var car2x = 10;
-  var car2y = 50;
+  var car2y = 90;
 
   $(document).on('keypress', function(event) {
     if (event.keyCode == 13) {
       console.log("you pressed a key!");
       ctx.clearRect(0,0,800,500);
       car1x = car1x+5;
-      car1 = ctx.fillRect(car1x,car1y,15,10);
-      car2 = ctx.fillRect(car2x,car2y,15,10);
-    }
+      car1 = ctx.fillRect(car1x,car1y,20,40);
+      car2 = ctx.fillRect(car2x,car2y,20,40);
+    };
+    if (event.keyCode == 100) {
+      console.log("you pressed a different key!");
+      ctx.clearRect(0,0,800,500);
+      car2x = car2x+5;
+      car2 = ctx.fillRect(car1x,car1y,20,40);
+      car1 = ctx.fillRect(car2x,car2y,20,40);
+    };
   });
 
 
