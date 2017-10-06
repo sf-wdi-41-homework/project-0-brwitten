d83e8414b64930db3d1839a2108abdac1eb26137
brwitten <bethrwitten@gmail.com>
2017-10-06 12:04:04 -0700

    moving rectangle on click

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ console.log("I'm linked!");
 
 $(document).ready(function(){
 
-  $('#redball').on('click', moveBall);
+  var c = document.getElementById('racetrack');
+  var ctx = c.getContext("2d");
+  var car1 = ctx.fillRect(10,10,15,10);
+  var car1x = 10;
+  var car1y = 10;
 
-  function moveBall(event) {
-    var id = event.currentTarget; // identifying DOM object that was clicked
-    var square = event.currentTarget.id; // storing specific ID that was clicked
-    console.log(event);
-    console.log(id);
-    console.log(square);
+  $('canvas').on('click', moveCar);
+
+  function moveCar(event) {
+    // var id = event.currentTarget; // identifying DOM object that was clicked
+    // var square = event.currentTarget.id; // storing specific ID that was clicked
+    // console.log(event);
+    // console.log(id);
+    // console.log(square);
     alert("hello!");
+    ctx.clearRect(0,0,800,500);
+    car1x = car1x+5;
+    car1 = ctx.fillRect(car1x,car1y,15,10);
   }
 
-});
 
-// need to create an object and place it on the screen
-// need to determine and update the objects position through event listeners
+
+// create pieces
+// move pieces
 // need to determine winning position
 // need to determine tie position
+
+
+});
