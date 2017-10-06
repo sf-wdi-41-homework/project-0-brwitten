b919c1002d3ead178e95c690244ef8bb9e1943f9
brwitten <bethrwitten@gmail.com>
2017-10-06 13:34:23 -0700

    winning conditions defined

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log("I'm linked!");
 
 $(document).ready(function(){
 
+// create board and initial pieces
+
   var c = document.getElementById('racetrack');
   var ctx = c.getContext("2d");
   var car1 = ctx.fillRect(10,20,20,40);
@@ -11,27 +13,38 @@ $(document).ready(function(){
   var car2x = 10;
   var car2y = 90;
 
+// move pieces with the return and D keypress
+
   $(document).on('keypress', function(event) {
-    if (event.keyCode == 13) {
+
+  // code to move the top object -- car1
+    if (event.keyCode == 13) { // this is return
       console.log("you pressed a key!");
       ctx.clearRect(0,0,800,500);
       car1x = car1x+5;
       car1 = ctx.fillRect(car1x,car1y,20,40);
       car2 = ctx.fillRect(car2x,car2y,20,40);
     };
-    if (event.keyCode == 100) {
+
+  // code to move the bottom object -- car 2
+    if (event.keyCode == 100) { // this is D
       console.log("you pressed a different key!");
       ctx.clearRect(0,0,800,500);
       car2x = car2x+5;
       car2 = ctx.fillRect(car1x,car1y,20,40);
       car1 = ctx.fillRect(car2x,car2y,20,40);
     };
+    console.log(car2x);
+    if (car2x == 280) {
+      alert("bottom car wins!");
+    }
+    if (car1x == 280) {
+      alert("top car wins!");
+    }
   });
 
-
-// create pieces
-// move pieces
 // need to determine winning position
+
 // need to determine tie position
 
 
